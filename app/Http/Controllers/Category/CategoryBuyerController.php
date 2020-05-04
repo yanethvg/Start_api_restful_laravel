f1598c9cd86f33e6888719d9f5f408272505702e
Zoila <zoilavillatoro6694@outlook.com>
2020-05-04 11:52:24 -0600

    added middleware auth:api in controller

--- app/Http/Controllers/Category/CategoryBuyerController.php
+++ app/Http/Controllers/Category/CategoryBuyerController.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\ApiController;
 
 class CategoryBuyerController extends ApiController
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
      public function index(Category $category)
      {
         
